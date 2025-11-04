9ac856e057cd2e039fba3b4a2719276dff1fb7a8
Zoevalo65325 <zoevalor45@gmail.com>
2025-11-03 20:29:26 -0500

    Expand forbidden words list and enhance comments

--- seguridad.js
+++ seguridad.js
@@ -15,12 +15,41 @@ function normalizaTexto(t) {
     .replace(/[^a-z0-9]/g, '');
 }
 
-// Palabras ofensivas (puedes añadir más)
+// Palabras ofensivas, insultos, negativas y polémicas (ampliado por país, región y diminutivos)
 const forbiddenWords = [
-  'puta','mierda','verga','pendejo','culero','fuck','shit','bitch','asshole','idiot','joto','puto'
+  // Groserías fundamentales
+  "puta","puto","put@", "putita","putito",
+  "mierda","verga","coño","gilipollas","pendejo","pendeja","culero","culera",
+  "culiao","zorra","zorrilla","carajo","cabrón","cabrona","imbecil","imbécil",
+  "idiota","idiotita","estúpido","estupido","estúpida","estupida","marica","maricón","maricona",
+  "mariconazo","huevon","huevón","huevona","huevones","perra","perrito","lame","papanatas",
+  "gonorrea","gonorreita","guevón","guevona","guevones","vreta","boludo","boluda","pelotudo",
+  "pelotuda","concha","culia","culiá","culeado","culeada","culiado","culiada","mongol","mongolo",
+  "mongólica","mongolico","retrasado","autista","down",
+  // Inglesas y globalizadas
+  "fuck","shit","ass","butt","bitch","asshole","dick","dumb","bastard","slut",
+  "faggot","bollocks","motherfucker","jerk","dickhead","sucker","loser","wanker",
+  // Discriminación y aspectos físicos
+  "gordo","gorda","flaco","flaca","obeso","fea","feo","negro","negra","cholo","china","enano","enana",
+  "petiso","petisa","retardado","retrasado","lisiado","cojo","coja","cojito","cojita",
+  // Ofensivas comunes y diminutivos típicos
+  "tonto","tonta","tontito","tontita","tontuelo","tontuela","tontorrón","tontorron",
+  "baboso","babosa","babosito","babosilla","bobo","boba","bobito","bobilla",
+  "burro","burra","burrito","burrita",
+  "torpe","torpón","torpona","tarado","tarada","taradito",
+  "pesado","pesada","pesadito","pesadita",
+  "loco","loca","loquito","loquita","patan","patán","patancito",
+  // Regionalismos fuertes (expande a tu gusto)
+  "chingada","chingar","pinche","cabron","joputa","csm","ctm","mierdoso","mierdosa",
+  // Más insultos frecuentes
+  "animal","bestia","asno","bruja","brujo","cerda","cerdo","tarántula","lagarta","lagarto","parásito",
+  "bruji","ñero","ñera","menso","mensito","mensita","imbecilucho",
+  // Palabras sociales usadas como insulto
+  "maldito","maldita","inútil","inutil","aburrido","aburrida","malcriado","malcriada","grosero","grosera",
+  "payaso","payasa","ridículo","ridícula","ridiculillo","ridiculilla","noob","novato","novata"
 ];
 
-// Revisa si hay alguna grosería en el texto
+// Revisa si hay alguna grosería o insulto en el texto
 function contieneGroserias(texto) {
   texto = normalizaTexto(texto);
   return forbiddenWords.some(word => texto.includes(normalizaTexto(word)));
@@ -50,8 +79,12 @@ document.addEventListener('contextmenu', function(evt) {
   evt.preventDefault();
 }, false);
 
-// Bloquea copiar (si lo deseas, borra si no lo quieres)
+// Bloquea copiar/cortar (puedes comentar si no lo deseas)
 document.addEventListener('copy', function(e) {
   e.preventDefault();
   alert('¡No se permite copiar!');
+});
+document.addEventListener('cut', function(e) {
+  e.preventDefault();
+  alert('¡No se permite cortar!');
 });
